991a29c85e2dbf50674f87144d0cdc86669f4373
Maria Soria <maria.it3@gmail.com>
2023-04-25 10:01:13 +0100

    First commit

---
+++ c-sharp-boilerplate/Greeting.cs
@@ -0,0 +1,10 @@
+namespace c_sharp_boilerplate
+{
+    public class Greeting
+    {
+        public static string sayHello()
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ c-sharp-boilerplate/GreetingTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace c_sharp_boilerplate
+{
+    [TestFixture]
+    public class GreetingTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreSame(Greeting.sayHello(), "Hello World!");
+        }
+    }
+}
